bf82b6f6da9e37503248d1c1b8dcd56ff776a222
Light2288 <davide.aliti@gmail.com>
2022-08-25 00:31:26 +0200

    add scene add crash sequence and finish sequence with delay

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,9 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    private Movements _movements;
+    [SerializeField] private float loadDelay = 2f;
+    void Start()
+    {
+        _movements = GetComponent<Movements>();
+    }
+    
     private void OnCollisionEnter(Collision collision)
     {
         switch (collision.gameObject.tag)
@@ -13,11 +21,38 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("Friendly");
                 break;
             case "Finish":
-                Debug.Log("Finish");
+                StartSuccessSequence();
                 break;
             default:
-                Debug.Log("Other");
+                StartCrashSequence();
                 break;
         }
+    }
+
+    private void StartSuccessSequence()
+    {
+        _movements.enabled = false;
+        Invoke(nameof(LoadNextLevel), loadDelay);
+    }
+
+    private void StartCrashSequence()
+    {
+        _movements.enabled = false;
+        Invoke(nameof(ReloadLevel), loadDelay);
+    }
+
+    private void ReloadLevel()
+    {
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    private void LoadNextLevel()
+    {
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        var lastSceneIndex = SceneManager.sceneCountInBuildSettings;
+        var sceneIndexToLoad = currentSceneIndex == lastSceneIndex - 1 ? 0: currentSceneIndex + 1;
+        SceneManager.LoadScene(sceneIndexToLoad);
+
     }
 }
