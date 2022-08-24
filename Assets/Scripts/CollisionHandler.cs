dc79ab8594b8ca995d2ebb640330e0516604947f
Light2288 <davide.aliti@gmail.com>
2022-08-24 15:57:46 +0200

    add collision handler

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Friendly");
+                break;
+            case "Finish":
+                Debug.Log("Finish");
+                break;
+            default:
+                Debug.Log("Other");
+                break;
+        }
+    }
+}
